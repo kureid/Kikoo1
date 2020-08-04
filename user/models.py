f5242aabf3c9506707d42f068c10c9073829167e
Ryo <hana05ry@icloud.com>
2020-08-04 08:29:05 +0000

    recommit

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.urils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    author = 
