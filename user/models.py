892795bdd40bef9042063fab8e45520bfd5d7dde
Ryo <hana05ry@icloud.com>
2020-08-04 09:27:25 +0000

    Kikoo chap3 Fin

--- user/models.py
+++ user/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-from django.urils import timezone
-from django.contrib.auth.models import User
-
-
-class Post(models.Model):
-    content = models.TextField()
-    author = 
